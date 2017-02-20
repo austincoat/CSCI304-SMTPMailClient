3f873561f27d314d30be91405d75683c26c0ec8e
Austin Coates <austincoat@gmail.com>
2017-02-19 21:22:17 -0500

    Updated GUI

--- emailClient.py
+++ emailClient.py
@@ -1,85 +1,93 @@
 import sys
 from socket import *
-from PyQt4 import QtCore, QtGui, uic
+from Tkinter import *
 
-qtCreatorFile = "emailGUI.ui"
+#!/usr/bin/python
+def sendEmail():
+    mailserver = T.get("1.0",END)
+    receive = T2.get("1.0",END)
+    cc = T3.get("1.0",END)
+    subject = T4.get("1.0",END)
+    body = T5.get("1.0",END)
+    clientSocket = socket(AF_INET, SOCK_STREAM)
+    clientSocket.connect((mailserver, 25))
+    recvconnect = clientSocket.recv(1024)
+    print recvconnect
 
-Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+    if recvconnect[:3] != '220':
+        print '220 reply not received from server.'
 
-class MyApp(QtGui.QMainWindow, Ui_MainWindow):
-    def __init__(self):
-        QtGui.QMainWindow.__init__(self)
-        Ui_MainWindow.__init__(self)
-        self.setupUi(self)
+    #Send HELO command and print server response
+    heloCommand = 'HELO Alice \r\n'
+    clientSocket.send(heloCommand)
+    recv1 = clientSocket.recv(1024)
+    print recv1
+    # Include This everytime there is no reply
+    if recv1[:3] != '250':
+        print '250 reply not received from server.'
 
-if __name__ == "__main__":
-    app = QtGui.QApplication(sys.argv)
-    window = MyApp()
-    window.show()
-    sys.exit(app.exec_())
-
-msg = '\r\n I Love computer networks!'
-endmshg= "\r\n.\r\n"
-mailserver = 'www.000webhost.com'
-clientSocket = socket(AF_INET, SOCK_STREAM)
-clientSocket.connect((mailserver, 25))
-recvconnect = clientSocket.recv(1024)
-print recvconnect
-
-if recvconnect[:3] != '220':
-	print '220 reply not received from server.'
-
-#Send HELO command and print server response
-heloCommand = 'HELO Alice \r\n'
-clientSocket.send(heloCommand)
-recv1 = clientSocket.recv(1024)
-print recv1
-# Include This everytime there is no reply
-if recvhelo[:3] != '250':
+    #Send MAIL FROM command and print server response.
+    clientSocket.send("MAIL From: alcoates@delenn.artifice\r\n")
+    recv2 = clientSocket.recv(1024)
+    if recv2[:3] != '250':
 	print '250 reply not received from server.'
 
-#Set up Some Security
-clientSocket.send("AUTH PLAIN password\r\n")
-recv2 = clientSocket.recv(1024)
-print recv2
-if recv2[:3] != '250':
+    #Send RCPT TO command and print server response.
+    print "Sending RCPT TO Command"
+    clientSocket.send("RCPT TO:" + receive +"\r\n")
+    recv2 = clientSocket.recv(1024)
+    if recv2[:3] != '250':
 	print '250 reply not received from server.'
-
-
-#Send MAIL FROM command and print server response.
-clientSocket.send("MAIL From: austincoat@gmail.com\r\n")
-recv2 = clientSocket.recv(1024)
-if recv2[:3] != '250':
+    #Send DATA command and print server response.
+    print "Sending DATA Command"
+    clientSocket.send("DATA\r\n")
+    recv2 = clientSocket.recv(1024)
+    print recv2
+    if recv2[:3] != '250':
 	print '250 reply not received from server.'
-
-#Send RCPT TO command and print server response.
-print "Sending RCPT TO Command"
-clientSocket.send("RCPT TO: alcoates@stetson.edu\r\n")
-recv2 = clientSocket.recv(1024)
-if recv2[:3] != '250':
-	print '250 reply not received from server.'
-
-#Send DATA command and print server response.
-print "Sending DATA Command"
-clientSocket.send("DATA\r\n")
-recv2 = clientSocket.recv(1024)
-print recv2
-if recv2[:3] != '250':
-	print '250 reply not received from server.'
-
+        
 
 #Send Data and print server response.
-print "Sending Data"
-clientSocket.send("SUBJECT: SMTP Mail Client Test\nSMTP Mail Client Test\n.\n\r\n")
-recv2 = clientSocket.recv(1024)
-print recv2
-if recv2[:3] != '250':
+    print "Sending Data"
+    clientSocket.send("SUBJECT:"+subject+ "\n"+ body +"\n.\r\n")
+    recv2 = clientSocket.recv(1024)
+    print recv2
+    if recv2[:3] != '250':
 	print '250 reply not received from server.'
-
+        
 #Send QUIT command and get server response.
-clientSocket.send("QUIT\r\n")
-recv2 = clientSocket.recv(1024)
-if recv2[:3] != '250':
-	print '250 reply not received from server.'
+    clientSocket.send("QUIT\r\n")
+    recv2 = clientSocket.recv(1024)
+    if recv2[:3] != '250':
+        print '250 reply not received from server.'
+        
+    print "Email Sent!"
+    
 
-print "Email Sent!"
+top = Tk()
+# Code to add widgets will go here...
+w = Label(top, text= "Local Server")
+w2 = Label(top, text="Recepient")
+w3 = Label(top, text="CC")
+w4 = Label(top, text="Subject")
+w5 = Label(top, text="Body")
+T = Text(top, height=2, width=30)
+T2 = Text(top,height=3, width=30)
+T3 = Text(top,height=2, width=30)
+T4 = Text(top,height=2, width=30)
+T5 = Text(top,height=2,width=30)
+b1= Button(top, text='Send', width=25, command= sendEmail)
+b2= Button(top, text='Cancel',width=25, command= top.destroy)
+w.pack()
+T.pack()
+w2.pack()
+T2.pack()
+w3.pack()
+T3.pack()
+w4.pack()
+T4.pack()
+w5.pack()
+T5.pack()
+b1.pack()
+b2.pack()
+top.mainloop()
